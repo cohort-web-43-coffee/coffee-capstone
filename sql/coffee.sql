5ca8a590385d326f7cf8ecd9d1f9cb00af8745a6
Mariposa Wheat <mwheat3@cnm.edu>
2023-10-13 09:51:12 -0600

    added account table

--- sql/coffee.sql
+++ sql/coffee.sql
@@ -1,1 +1,8 @@
-dhfsdasgdfsasfassf
+
+
+create table if not exists account (
+    account_id uuid not null primary key,
+    account_email varchar(50) not null unique,
+    account_hash char(30) not null,
+    account_name varchar(25) not null unique
+)
