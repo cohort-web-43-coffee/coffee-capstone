110aaf5cef49b28544c718576995680b55954de8
Joshua Yu <jyu6@cnm.edu>
2023-10-13 10:38:30 -0600

    made more tables and drop statements huzzah

--- sql/coffee.sql
+++ sql/coffee.sql
@@ -1,4 +1,9 @@
-
+drop table if exists account cascade;
+drop table if exists shop cascade;
+drop table if exists photo cascade;
+drop table if exists tag cascade;
+drop table if exists active_tag cascade;
+drop table if exists bookmark cascade;
 
 create table if not exists account (
     account_id uuid not null primary key,
@@ -18,7 +23,7 @@ create table if not exists photo (
     photo_shop_id uuid not null,
     photo_credit varchar (512) not null,
     photo_description varchar (512) not null,
-    photo_order date not null,
+    photo_order integer not null,
     photo_url varchar(512) null,
     foreign key (photo_shop_id) references shop (shop_id)
 );
@@ -30,5 +35,27 @@ create table if not exists tag (
     tag_group varchar(512) not null,
     tag_label varchar(512) not null
 );
+
+create table if not exists active_tag(
+    active_tag_account_id uuid not null,
+    active_tag_shop_id uuid not null,
+    active_tag_tag_id uuid not null,
+    foreign key (active_tag_account_id) references account(account_id),
+    foreign key (active_tag_shop_id) references shop(shop_id),
+    foreign key (active_tag_tag_id) references tag(tag_id)
+);
+create index on active_tag(active_tag_account_id);
+create index on active_tag(active_tag_shop_id);
+create index on active_tag(active_tag_tag_id);
+
+create table if not exists bookmark (
+    bookmark_account_id uuid not null,
+    bookmark_shop_id uuid not null,
+    bookmark_order integer not null,
+    foreign key (bookmark_account_id) references account(account_id),
+    foreign key (bookmark_shop_id) references shop(shop_id)
+);
+create index on bookmark(bookmark_account_id);
+create index on bookmark(bookmark_shop_id);
 
 
