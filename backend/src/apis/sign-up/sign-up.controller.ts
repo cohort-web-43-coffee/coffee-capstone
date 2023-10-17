bb80eef0dbdd3926ed15696a14ef8fbda7565598
Joshua Yu <jyu6@cnm.edu>
2023-10-17 13:51:33 -0600

    made the sign-up controller

---
+++ backend/src/apis/sign-up/sign-up.controller.ts
@@ -0,0 +1,43 @@
+import {Request, Response} from 'express'
+import {SignUpSchema} from "./sign-up.validator";
+import {zodErrorResponse} from "../../utils/response.utils";
+import {setHash} from "../../utils/auth.utils";
+import {insertAccount, PrivateAccount} from "../account/account.model";
+import {Status} from "../../utils/interfaces/Status";
+
+
+
+
+
+
+
+export async function signUpController (request: Request, response: Response): Promise<Response | undefined> {
+    try{
+        const bodyValidationResult = SignUpSchema.safeParse(request.body)
+        if(!bodyValidationResult.success) {
+            return zodErrorResponse(response, bodyValidationResult.error)
+        }
+        const {accountEmail, accountName, accountPassword} = bodyValidationResult.data
+        const accountHash = await setHash(accountPassword)
+        const account: PrivateAccount = {
+            accountId: null,
+            accountEmail,
+            accountHash,
+            accountName
+        }
+        await insertAccount(account)
+        const status: Status = {
+            status: 200,
+            message: 'Account successfully created please check your email.',
+            data: null
+        }
+        return response.json(status)
+    } catch (error: any) {
+        const status: Status = {
+            status: 500,
+            message: error.message,
+            data: null
+        }
+        return response.json(status)
+    }
+}
