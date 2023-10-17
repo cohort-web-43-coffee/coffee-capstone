a2c639f6aa6fb9dabad5ca1ce3cd3f94f693458a
Joshua Yu <jyu6@cnm.edu>
2023-10-17 13:29:30 -0600

    added sign-up controller

---
+++ backend/src/apis/sign-up/sign-up.controller.ts
@@ -0,0 +1,16 @@
+import {z} from 'zod'
+import {PrivateAccountSchema} from "../account/account.validator"
+
+export const SignUpSchema = PrivateAccountSchema
+    .omit({accountHash: true})
+    .extend({
+        accountPasswordConfirm: z.string()
+            .min(8, {message: 'please provide a valid password for your account (min. 8 characters)'})
+            .max(64, {message: 'please provide a valid password for your account (max. 64 characters)'}),
+        accountPassword: z.string()
+            .min(8, {message: 'please provide a valid password for your account (min. 8 characters)'})
+            .max(64, {message: 'please provide a valid password for your account (max. 64 characters)'})
+    })
+    .refine(data => data.accountPassword === data.accountPasswordConfirm,
+        {message: 'passwords do not match. Please fix this.'}
+    )
