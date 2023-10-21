36510a975a1b68b7f2e074c914617118b2b45586
Joshua Yu <jyu6@cnm.edu>
2023-10-20 20:02:08 -0600

    fixed the route for getting shops by id

--- backend/src/apis/shop/shop.route.ts
+++ backend/src/apis/shop/shop.route.ts
@@ -6,7 +6,7 @@ const basePath = '/apis/shop'
 const router = Router()
 router.route('/')
     .get(getAllShopsController)
-router.route(':shopId')
+router.route('/:shopId')
     .get(getShopByShopIdController)
 
 export const shopRoute = {basePath, router}
