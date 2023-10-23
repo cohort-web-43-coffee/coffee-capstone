b107b26978f4b79124015ef5fce42edf58c9b8b6
Joshua Yu <jyu6@cnm.edu>
2023-10-23 15:39:20 -0600

    removed the nullable

--- backend/src/apis/tags/tag.validator.ts
+++ backend/src/apis/tags/tag.validator.ts
@@ -3,8 +3,7 @@ import {z} from "zod";
 
 export const TagSchema = z.object({
     tagId: z.string({required_error: 'please provide a tagId'})
-        .uuid({message: 'please provide a valid uuid for tagId'})
-        .nullable(),
+        .uuid({message: 'please provide a valid uuid for tagId'}),
     tagGroup: z.string({required_error: 'please provide a tagGroup'})
         .min(1, {message: 'tagGroup must be at least 1 character. Thank you.'})
         .max(512, {message: 'tagGroup must be at most 512 character. Thank you'}),
