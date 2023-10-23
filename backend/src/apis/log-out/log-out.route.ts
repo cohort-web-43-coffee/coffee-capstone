5ad7bbdb1e7a930aad1ca930b4b17efc3ad31392
victor villanueva <vvillanueva2@cnm.edu>
2023-10-23 14:54:25 -0600

    sign-out works

--- backend/src/apis/log-out/log-out.route.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import {logOutController} from "./log-out.controller";
-
-
-const basePath = '/apis/log-out'
-const router = Router()
-router.route('/').get(logOutController)
-
-export const logOutRoute = {basePath, router}
