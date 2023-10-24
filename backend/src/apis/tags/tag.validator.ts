455e1f939faea4aa0912e403a028b036e455592a
Joshua Yu <jyu6@cnm.edu>
2023-10-24 16:59:29 -0600

    undid tag id not null

--- backend/src/apis/tags/tag.validator.ts
+++ backend/src/apis/tags/tag.validator.ts
@@ -3,7 +3,8 @@ import {z} from "zod";
 
 export const TagSchema = z.object({
     tagId: z.string({required_error: 'please provide a tagId'})
-        .uuid({message: 'please provide a valid uuid for tagId'}),
+        .uuid({message: 'please provide a valid uuid for tagId'})
+        .nullable(),
     tagGroup: z.string({required_error: 'please provide a tagGroup'})
         .min(1, {message: 'tagGroup must be at least 1 character. Thank you.'})
         .max(512, {message: 'tagGroup must be at most 512 character. Thank you'}),
