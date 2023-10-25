58530ea9d9a53eea496fa099a8678cf968e988fa
victor villanueva <vvillanueva2@cnm.edu>
2023-10-25 11:54:06 -0600

    added photo_id primary key sql,updated controller, route, validator and model

--- backend/src/apis/photo/photo.validator.ts
+++ backend/src/apis/photo/photo.validator.ts
@@ -1,12 +1,18 @@
 import {z} from "zod"
 export const PhotoSchema = z.object({
-    photo_shop_id: z.string(
-        {required_error: 'please provide a valid photoId or null.'})
-        .uuid({message: 'please provide a valid uuid for shopId'})
+    photo_id: z.string(
+        {required_error: 'please provide a valid photoId or null'})
+        .uuid({message: 'please provide a valid uuid for photoId'})
         .nullable(),
+    photo_shop_id: z.string(
+        {required_error: 'please provide a valid photoShopId'})
+        .uuid({message: 'please provide a valid uuid for photoshopId'}),
     photo_credit: z.string(),
     photo_description: z.string(),
     photo_order: z.number(),
-    photo_url: z.string()
+    photo_url: z.string({required_error: 'please provide a valid threadImageUrl or null'})
+        .trim()
+        .url({message: 'please provide a valid URL for threadImageUrl'})
+        .max(255, {message: 'please provide a valid threadImageUrl (max 255 characters)'})
         .nullable(),
 });
