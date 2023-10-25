1aa7d6332e2d0e396376da2de9299d650a502453
Bob Liberatore <bob@inhale.design>
2023-10-25 10:55:13 -0600

    Get accountId from session for bookmark

--- backend/src/apis/bookmark/bookmark.validator.ts
+++ backend/src/apis/bookmark/bookmark.validator.ts
@@ -2,7 +2,14 @@ import {z} from 'zod';
 
 
 export const BookmarkSchema = z.object({
-    bookmarkAccountId: z.string({required_error: 'please provide a valid bookmarkAccountId'}).uuid({message: 'please provide a valid uuid for bookmarkAccountId'}),
-    bookmarkShopId: z.string({required_error: 'please provide a valid bookmarkAccountId'}).uuid({message: 'please provide a valid uuid for bookmarkShopId'}),
+    bookmarkAccountId: z.string(
+        {required_error: 'please provide a valid bookmarkAccountId'}
+    )
+        .uuid({message: 'please provide a valid uuid for bookmarkAccountId'})
+        .nullable(),
+    bookmarkShopId: z.string(
+        {required_error: 'please provide a valid bookmarkAccountId'}
+    )
+        .uuid({message: 'please provide a valid uuid for bookmarkShopId'}),
     bookmarkOrder: z.number({required_error: 'please provide a valid bookmarkOrder'}).int()
 })
