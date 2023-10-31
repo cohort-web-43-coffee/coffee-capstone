8aa5100543388e05b2509d4c99b7495c2339a70d
Bob Liberatore <bob@inhale.design>
2023-10-31 15:30:40 -0600

    Add custom group and add button, move mock data

--- frontend/src/app/shop/page.tsx
+++ frontend/src/app/shop/page.tsx
@@ -1,64 +1,14 @@
-import {PrimarySection, SecondarySection} from '@/app/components/Sections'
+import {SecondarySection} from '@/app/components/Sections'
 import React from 'react'
 import {Container} from '@/app/components/Container'
+import {busyTags, customTags, drinkTags} from '@/app/mocks/tags'
 
 type Tag = { label: string, count: number }
 type TagGroup = { group: string, tags: Tag[] }
 type TagProps = { tag: Tag }
-type TagGroupProps = { group: TagGroup }
+type TagGroupProps = { group: TagGroup, children?: any }
 
-const drinkTags =
-    {
-        group: 'Drinks',
-        tags: [
-            {
-                label: 'Espresso',
-                count: 9
-            },
-            {
-                label: 'Drip',
-                count: 5
-            },
-            {
-                label: 'Pour-Over',
-                count: 8
-            },
-            {
-                label: 'Turkish',
-                count: 2
-            }
-        ]
-    }
-const busyTags =
-    {
-        group: 'Busy',
-        tags: [
-            {
-                label: 'Sunrise',
-                count: 2
-            },
-            {
-                label: 'Morning',
-                count: 4
-            },
-            {
-                label: 'Lunch',
-                count: 12
-            },
-            {
-                label: 'Afternoon',
-                count: 8
-            },
-            {
-                label: 'Evening',
-                count: 2
-            },
-            {
-                label: 'Late',
-                count: 0
-            }
-        ]
-    }
+
 
 export default function ShopPage () {
     return <>
@@ -69,6 +19,9 @@ export default function ShopPage () {
 function ImagesSection () {
     return <SecondarySection>
         <Container autoMargins>
+            <TagList group={customTags}>
+                <button className={'btn btn-primary btn-xs rounded-full'}>New +</button>
+            </TagList>
             <TagList group={drinkTags}/>
             <TagList group={busyTags}/>
         </Container>
@@ -76,10 +29,10 @@ function ImagesSection () {
 }
 
 function TagList (props: TagGroupProps) {
-    const {group} = props
+    const {group, children} = props
     return <>
-        <div className={'divider'}>{group.group}</div>
-        <div className={'flex gap-6'}>
+        <div className={'divider'}>{group.group}{children}</div>
+        <div className={'flex flex-wrap gap-6 justify-around'}>
             {group.tags
                 .sort((a: Tag, b: Tag) => b.count - a.count)
                 .map((tag: Tag) => <TagButton tag={tag}/>)}
