4c2fd8749c04bd477947d60d134e8074ca79e100
victor villanueva <vvillanueva2@cnm.edu>
2023-11-02 13:26:05 -0600

    touch up components

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import {ContactForm} from "@/app/components/ContactForm";
+import ContactForm from "@/app/components/ContactForm";
 import {PrimarySection, SecondarySection} from '@/app/components/Sections'
 import {Container} from '@/app/components/Container'
 
@@ -18,17 +18,22 @@ export default function AboutPage() {
                 </Container>
 
             </SecondarySection>
+
         </>
     )
 }
 
 function AboutUs () {
     return (
-        <div className="basis-5/6 max-md:px-8">
-            <h1 className="text-6x md:text-6xl">About Us!</h1>
-            <p>Lucio fulci tremor est dark vivos magna. Expansis creepy arm yof darkness ulnis witchcraft missing carnem
-                armis Kirkman Moore and Adlard caeruleum in locis. Romero morbo Congress amarus in auras. Nihil horum
-                sagittis tincidunt, zombie slack-jawed gelida survival portenta.</p>
+        <div>
+             <div style={{ margin: 'auto', width: '50%' }}>
+                <div className={'prose'}>
+                    <h1>About Us!</h1>
+                    <p>Lucio fulci tremor est dark vivos magna. Expansis creepy arm yof darkness ulnis witchcraft missing carnem
+                        armis Kirkman Moore and Adlard caeruleum in locis. Romero morbo Congress amarus in auras. Nihil horum
+                        sagittis tincidunt, zombie slack-jawed gelida survival portenta.</p>
+                </div>
+             </div>
         </div>
 
     )
