7b682320da848ab8d5aca20a44b17ad2de90c751
Bob Liberatore <bob@inhale.design>
2023-11-03 16:11:05 -0600

    Refactor navbar

--- frontend/src/app/components/NavBar.tsx
+++ frontend/src/app/components/NavBar.tsx
@@ -2,53 +2,70 @@
 import Link from "next/link";
 import {SignUpModal} from "@/app/components/Modal";
 
+
 export function NavBar () {
     return (
-        <div className="navbar">
+        <nav className="navbar">
             <div className="dropdown">
-                <label tabIndex={0} className="btn btn-ghost md:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                              d="M4 6h16M4 12h8m-8 6h16"/>
-                    </svg>
-                </label>
+                <MenuButton/>
                 <ul className="menu menu-sm dropdown-content mt-3 z-50 p-2 shadow bg-base-100 rounded-box w-32">
-                    <li><Link href={"/"}>Home</Link></li>
-                    <li><Link href={"/account"}>Account</Link></li>
-                    <li><Link href={"/shop"}>Shops</Link></li>
-                    <li><Link href={"/about"}>About Us</Link></li>
-                    <li>
-                        <button
-                            onClick={() => (document.getElementById('sign-up-modal') as HTMLDialogElement).showModal()}
-                            className={'btn btn-primary btn-xs rounded-full'}>Sign up
-                        </button>
-                    </li>
+                    <MenuContent/>
                 </ul>
             </div>
             <div className="flex-1">
-                <p className="normal-case text-xl">Valid Coffee</p>
+                <SiteTitle/>
             </div>
             <div className="flex-none">
-                <div className="form-control">
-                    <input type="text" placeholder="Search" className="input input-bordered w-40 md:w-auto"/>
-                </div>
+                <SearchField/>
                 <div className="navbar-center hidden md:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><Link href={"/"}>Home</Link></li>
-                        <li><Link href={"/account"}>Account</Link></li>
-                        <li><Link href={"/shop"}>Shops</Link></li>
-                        <li><Link href={"/about"}>About Us</Link></li>
-                        <li>
-                            <button
-                                onClick={() => (document.getElementById('sign-up-modal') as HTMLDialogElement).showModal()}
-                                className={'btn btn-primary btn-xs rounded-full'}>Sign up
-                            </button>
-                        </li>
+                        <MenuContent/>
                     </ul>
                 </div>
             </div>
             <SignUpModal/>
+        </nav>
+    )
+}
+
+function SiteTitle () {
+    return <header className={'text-2xl'}>Valid Coffee</header>
+}
+
+function SearchField () {
+    return (
+        <div className="form-control">
+            <input type="text" placeholder="Search" className="input input-bordered w-40 md:w-auto"/>
         </div>
+    )
+}
+
+function MenuButton () {
+    return (
+        <label tabIndex={0} className="btn btn-ghost md:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                 stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                      d="M4 6h16M4 12h8m-8 6h16"/>
+            </svg>
+        </label>
+    )
+}
+
+function MenuContent () {
+    return (
+        <>
+            <li><Link href={'/'}>Home</Link></li>
+            <li><Link href={'/account'}>Account</Link></li>
+            <li><Link href={'/shop'}>Shops</Link></li>
+            <li><Link href={'/about'}>About Us</Link></li>
+            <li>
+                <button
+                    onClick={() => (document.getElementById('sign-up-modal') as HTMLDialogElement).showModal()}
+                    className={'btn btn-primary btn-xs rounded-full'}>
+                    Sign up
+                </button>
+            </li>
+        </>
     )
 }
