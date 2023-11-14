9844426de9136b16cea28e93e4798b421122364f
Bob Liberatore <bob@inhale.design>
2023-11-14 10:06:59 -0700

    Update REST with shop photo url

--- backend/src/apis/shop/shop.model.ts
+++ backend/src/apis/shop/shop.model.ts
@@ -10,7 +10,7 @@ export type Shop = z.infer<typeof ShopSchema>
  * @param shopId
  */
 export async function getShopByShopId (shopId: string): Promise<Shop | null> {
-    const rowList = <Shop[]>await sql`select shop_id, shop_address, shop_name, shop_phone_number, shop_url
+    const rowList = <Shop[]>await sql`select shop_id, shop_address, shop_name, shop_phone_number, shop_url, shop_photo_url
                                       from shop
                                       where shop_id = ${shopId}`
     const result = ShopSchema.array().max(1).parse(rowList)
@@ -19,14 +19,13 @@ export async function getShopByShopId (shopId: string): Promise<Shop | null> {
 
 
 export async function getAllShops (): Promise<Shop[]> {
-    const rowList = <Shop[]>await sql`select shop_id, shop_address, shop_name, shop_phone_number, shop_url
-                                      from shop
-                                      order by shop_name desc`
+    const rowList = <Shop[]>await sql`select shop_id, shop_address, shop_name, shop_phone_number, shop_url, shop_photo_url
+                                      from shop`
     return ShopSchema.array().parse(rowList)
 }
 
 export async function searchShopName (query: string): Promise<Shop[]> {
-    const rowList = <Shop[]>await sql`select shop_id, shop_address, shop_name, shop_phone_number, shop_url
+    const rowList = <Shop[]>await sql`select shop_id, shop_address, shop_name, shop_phone_number, shop_url, shop_photo_url
                                       from shop
                                       where shop_name %> ${query}`
     return ShopSchema.array().parse(rowList)
@@ -34,10 +33,10 @@ export async function searchShopName (query: string): Promise<Shop[]> {
 
 export async function getShopsWithTags (tagIds: string[]): Promise<Shop[]> {
     const sqlTagArray = stringArrayToSQLArrayLiteral(tagIds)
-    const rowList = <Shop[]>await sql`select shop_name, shop_address, shop_id, shop_phone_number, shop_url, array_agg(DISTINCT active_tag_tag_id)::uuid[]
+    const rowList = <Shop[]>await sql`select shop_name, shop_address, shop_id, shop_phone_number, shop_url, shop_photo_url, array_agg(DISTINCT active_tag_tag_id)::uuid[]
                                       from shop
                                                join public.active_tag on shop.shop_id = active_tag_shop_id
-                                      group by (shop_name, shop_address, shop_id, shop_phone_number, shop_url)
+                                      group by (shop_name, shop_address, shop_id, shop_phone_number, shop_url, shop_photo_url)
                                       having array_agg(DISTINCT active_tag_tag_id)::uuid[] @> ${sqlTagArray}`
     return ShopSchema.array().parse(rowList)
 }
