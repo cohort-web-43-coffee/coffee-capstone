c2e4a406bfe02307189512e46b1d2cc388056a7b
Joshua Yu <jyu6@cnm.edu>
2023-11-15 12:14:00 -0700

    clean the styling

--- frontend/src/app/layout/NavBar.tsx
+++ frontend/src/app/layout/NavBar.tsx
@@ -48,12 +48,8 @@ export function MenuContent() {
             <li><Link href={'/'}>Home</Link></li>
             <li><Link href={'/account'}>Account</Link></li>
             <li><Link href={'/about'}>About Us</Link></li>
-            <li>
-                <SignUpModalButton/>
-            </li>
-            <li>
-                <SignInModalButton/>
-            </li>
+            <li><SignUpModalButton/></li>
+            <li><SignInModalButton/></li>
         </>
     )
 }
