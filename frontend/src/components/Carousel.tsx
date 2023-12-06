59fe7e7424a17166993a7dd0cdeb2a08c38578a4
Bob Liberatore <bob@inhale.design>
2023-12-06 11:42:18 -0700

    Fix scrolling regression

--- frontend/src/components/Carousel.tsx
+++ frontend/src/components/Carousel.tsx
@@ -33,8 +33,8 @@ export function CarouselSlide({slideId, nextSlideId, previousSlideId, children, 
 function CarouselNav({previousSlideId, nextSlideId}: Readonly<CarouselNavProps>) {
     return (
         <nav className={'absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'}>
-            <Link href={`#${previousSlideId}`} className={'btn btn-circle'} replace scroll={false}>❮</Link>
-            <Link href={`#${nextSlideId}`} className={'btn btn-circle'} replace scroll={false}>❯</Link>
+            <Link href={`#${previousSlideId}`} className={'btn btn-circle'} replace>❮</Link>
+            <Link href={`#${nextSlideId}`} className={'btn btn-circle'} replace>❯</Link>
         </nav>
     )
 }
